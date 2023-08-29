5f754fc0307e72cc279a47ab1efeeb1eb86f2b80
Protract123 <vermayuvraj2010@gmail.com>
2023-08-29 13:47:03 +0530

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace UltraAchievements_Revamped
+{
+    [BepInPlugin("protract.ultrakill.ultra_achievements_revamped", "UltraAchievements", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
