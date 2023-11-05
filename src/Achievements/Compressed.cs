50effb3ec9c33180c96f003514acb5a676ae8d15
Yuvraj Verma <vermayuvraj2010@gmail.com>
2023-11-05 18:03:54 +0530

    Compressed

---
+++ src/Achievements/Compressed.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace UltraAchievements_Revamped.Achievements;
+[HarmonyPatch(typeof(StyleHUD), nameof(StyleHUD.AddPoints))]
+[RegisterAchievement("ultraAchievements.compressed")]
+public class Compressed
+{
+    public static void Postfix(string pointID)
+    {
+        if (pointID == "ultrakill.compressed")
+        {
+            AchievementManager.MarkAchievementComplete(AchievementManager.GetAchievementInfo(typeof(Compressed)));
+        }
+    }
+}
