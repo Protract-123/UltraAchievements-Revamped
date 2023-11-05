24a60ee4b827065c30d4932c732d7c722659b3c8
Yuvraj Verma <vermayuvraj2010@gmail.com>
2023-11-05 22:16:42 +0530

    Catapulted

---
+++ src/Achievements/Catapulted.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace UltraAchievements_Revamped.Achievements;
+[HarmonyPatch(typeof(StyleHUD), nameof(StyleHUD.AddPoints))]
+[RegisterAchievement("ultraAchievements.catapulted")]
+public class Catapulted
+{
+    public static void Postfix(string pointID)
+    {
+        if (pointID == "ultrakill.catapulted")
+        {
+            AchievementManager.MarkAchievementComplete(AchievementManager.GetAchievementInfo(typeof(Catapulted)));
+        }
+    }
+}
