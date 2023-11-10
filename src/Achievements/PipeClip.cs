41967a881e4c90857a4bcf511c04af9b412e182e
Yuvraj Verma <vermayuvraj2010@gmail.com>
2023-11-10 12:08:51 +0530

    Pipe Clip Partial Comletion

---
+++ src/Achievements/PipeClip.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace UltraAchievements_Revamped.Achievements;
+
+[HarmonyPatch(typeof(HudMessage), "PlayMessage")]
+[RegisterAchievement("ultraAchievements.pipeclip")]
+public class PipeClip
+{
+    public static void Postfix(HudMessage __instance)
+    {
+        if (__instance.message.Contains("PIPE CLIP"))
+        {
+            AchievementManager.MarkAchievementComplete(AchievementManager.GetAchievementInfo(typeof(PipeClip)));
+        }
+    }
+}
