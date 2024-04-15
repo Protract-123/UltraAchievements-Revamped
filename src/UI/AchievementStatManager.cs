4cb324ea6adba09c058838d99c9343c1af1de133
Yuvraj Verma <vermayuvraj2010@gmail.com>
2024-04-15 21:55:42 +0530

    Make Terminal stats accurate

---
+++ src/UI/AchievementStatManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UltraAchievements_Revamped.UI;
+
+public class AchievementStatManager : MonoBehaviour
+{
+    public Text completedAchievements;
+    public Text totalAchievements;
+    public Text miniGameScore;
+
+    private void Start()
+    {
+        int achievements = 0;
+        int compAchievements = 0;
+        int miniGameScore = 0;
+            
+        foreach (AchievementInfo info in AchievementManager.IdToAchInfo.Values)
+        {
+            if (info.isCompleted)
+            {
+                compAchievements++;
+            }
+
+            achievements++;
+        }
+
+        totalAchievements.text = $"<color=orange>{achievements}</color> - COMPLETED ACHIEVEMENTS";
+        completedAchievements.text = $"<color=orange>{compAchievements}</color> - TOTAL ACHIEVEMENTS";
+        
+        //TODO add mini game scores
+    }
+}
