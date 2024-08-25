7616b9281b8860a967d9f8f58b0616c2d74d1e79
Yuvraj Verma <vermayuvraj2010@gmail.com>
2024-08-25 19:11:45 +0530

    Ready for Version 1.1
    Testing not complete

---
+++ mod/Achievements/DoubleIt.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using UltraAchievements_Lib;
+
+namespace UltraAchievements_Revamped.Achievements;
+
+[RegisterAchievement("ultraAchievements.doubleit")]
+[HarmonyPatch]
+public class DoubleIt
+{
+    private static int _counter = 0;
+    
+    [HarmonyPatch(typeof(DualWield), "Start")]
+    public static void OnPowerUpGain()
+    {
+        _counter++;
+        if (_counter >= 4)
+        {
+            AchievementManager.MarkAchievementComplete(AchievementManager.GetAchievementInfo(typeof(DoubleIt)));
+        }
+    }
+
+    [HarmonyPatch(typeof(DualWield), nameof(DualWield.EndPowerUp))]
+    public static void OnPowerUpEnd()
+    {
+        _counter--;
+    }
+}
