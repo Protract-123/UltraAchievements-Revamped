0dfae31008b41149d82176bdff15d27de7367823
Yuvraj Verma <vermayuvraj2010@gmail.com>
2026-03-07 02:57:37 +0530

    src - initialize .NET projects

---
+++ src/UltraAchievementsRevamped.Mod/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace UltraAchievementsRevamped.Mod;
+
+[BepInPlugin("", "", "")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin is loaded!");
+    }
+}
