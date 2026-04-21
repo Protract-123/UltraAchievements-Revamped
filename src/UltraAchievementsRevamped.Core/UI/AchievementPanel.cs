e31a630b1dabcc3da65f11b7a34d1be64f57b956
Yuvraj Verma <vermayuvraj2010@gmail.com>
2026-04-21 20:01:51 +0530

    unity - small changes to Achievement Panel

--- src/UltraAchievementsRevamped.Core/UI/AchievementPanel.cs
+++ src/UltraAchievementsRevamped.Core/UI/AchievementPanel.cs
@@ -15,7 +15,7 @@ public class AchievementPanel : MonoBehaviour
     public GameObject achievementTemplate;
 
     public ShopButton backButton;
-    
+
     public int itemsPerPage;
     private readonly List<GameObject> modButtons = [];
     private readonly List<Button> pageButtons = [];
@@ -39,6 +39,7 @@ public class AchievementPanel : MonoBehaviour
         foreach ((string modName, List<AchievementInfo> achievements) in AchievementManager.ModNameToAchInfo)
             CreateModButton(modName, achievements);
 
+
         GoToPage(0);
     }
 
@@ -47,10 +48,10 @@ public class AchievementPanel : MonoBehaviour
         Transform parent = transform.Find("Left Panel/Buttons/Mod Buttons");
         GameObject modButton = Instantiate(modButtonTemplate, parent);
         modButton.GetComponentInChildren<TMP_Text>().text = modName;
-        
+
         int panelIndex = modButtons.Count;
         modButton.GetComponent<Button>().onClick.AddListener(() => SelectModPanel(panelIndex));
-        
+
         CreateModPanel(modName, achievements);
         modButtons.Add(modButton);
     }
@@ -61,8 +62,8 @@ public class AchievementPanel : MonoBehaviour
         modPanel.transform.Find("Title").GetComponent<TMP_Text>().text = modName;
         modPanel.SetActive(false);
 
-        Transform achievementContent = modPanel.transform.Find("Achievements/Viewport/Content");
-        
+        Transform achievementContent = modPanel.transform.Find("Achievement List/Viewport/Content");
+
         foreach (AchievementInfo achievement in achievements)
         {
             GameObject entry = Instantiate(achievementTemplate, achievementContent);
@@ -71,16 +72,16 @@ public class AchievementPanel : MonoBehaviour
             entry.transform.Find("Description").GetComponent<TMP_Text>().text = achievement.Description;
             entry.transform.Find("Icon").GetComponent<Image>().sprite = achievement.Icon;
         }
-        
+
         modPanels.Add(modPanel);
     }
-    
+
     private void SelectModPanel(int index)
     {
         for (int i = 0; i < modPanels.Count; i++)
             modPanels[i].SetActive(i == index);
     }
-    
+
     private void GoToPage(int pageIndex)
     {
         int start = pageIndex * itemsPerPage;
@@ -92,14 +93,13 @@ public class AchievementPanel : MonoBehaviour
         }
 
         int totalPages = Mathf.CeilToInt((float)modButtons.Count / itemsPerPage);
-        
+
         for (int i = 0; i < pageButtons.Count; i++)
         {
             if (i >= totalPages)
             {
                 pageButtons[i].interactable = false;
                 pageButtons[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
-
             }
             else if (i == pageIndex)
             {
@@ -113,18 +113,18 @@ public class AchievementPanel : MonoBehaviour
             }
         }
     }
-    
+
     [HarmonyPatch(typeof(ShopZone), "Start")]
     [HarmonyPostfix]
     private static void AchievementPanelPatch(ShopZone __instance)
     {
         GameObject mainPanel = __instance.transform.Find("Canvas/Background/Main Panel").gameObject;
         AchievementPanel achievementPanel = Instantiate(Assets.AchievementPanel, mainPanel.transform);
-        
+
         GameObject terminalIcon = __instance.transform.Find("Canvas/Background/Icon").gameObject;
         terminalIcon.AddComponent<Button>().interactable = true;
         terminalIcon.GetComponent<Image>().raycastTarget = true;
-        
+
         ShopButton achievementPanelButton = terminalIcon.AddComponent<ShopButton>();
         achievementPanelButton.toActivate = [achievementPanel.gameObject];
         achievementPanelButton.toDeactivate = [];
